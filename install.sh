149cd1f7f4eb7e31f201c7c590a0fcd6dee6170a
Tammar Abdulaziz <130196123+Li0N77@users.noreply.github.com>
2024-08-29 14:49:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,4 +35,4 @@ fi
 echo "Setup complete!"
 
 echo "To run the subdomain enumeration script, use the following command:"
-echo "python lsubs.py <domain> [--api_key_virustotal <key>] [--api_key_securitytrails <key>] [--api_key_censys <key>] [--api_key_shodan <key>] [-f <file>] [-nf]"
+echo "python lsubs.py <domain> [--api_key_securitytrails <key>] [--api_key_censys <key>] [--api_key_shodan <key>] [-f <file>] [-nf]"
