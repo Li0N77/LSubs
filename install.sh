b39fdd4eb003d4a0809b133f18c1b7fa0ff1180d
Tammar Abdulaziz <130196123+Li0N77@users.noreply.github.com>
2024-08-29 17:21:44 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+echo "setup configuration"
+echo "{\n    \"SecurityTrails\": \"\",\n    \"Censys\": \"\",\n    \"Shodan\": \"\"\n}" > ~/.local/bin/lsubs-config.json
+chmod +x lsubs
+cp lsubs ~/.local/bin/
+
+echo "Installing Python packages..."
+pip install -r requirements.txt
+
+
+echo "Setup complete!"
+
+echo "To run the subdomain enumeration script, use the following command:"
+echo "python lsubs.py <domain> [--api_key_virustotal <key>] [--api_key_securitytrails <key>] [--api_key_censys <key>] [--api_key_shodan <key>] [-f <file>] [-nf]"
