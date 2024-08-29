72c72fb8e624de538f08c2f07269258931fea4a3
Tammar Abdulaziz <130196123+Li0N77@users.noreply.github.com>
2024-08-29 17:33:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ pip install -r requirements.txt
 echo "Setup complete!"
 
 echo "To run the subdomain enumeration script, use the following command:"
-echo "python lsubs.py <domain> [--api_key_virustotal <key>] [--api_key_securitytrails <key>] [--api_key_censys <key>] [--api_key_shodan <key>] [-f <file>] [-nf]"
+echo "lsubs [-d <domain>] [--api_key_virustotal <key>] [--api_key_securitytrails <key>] [--api_key_censys <key>] [--api_key_shodan <key>] [--api <api arguments>]"
