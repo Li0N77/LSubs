857c34a0c0e67cb3c40237e169c1a505a398f6ac
Tammar Abdulaziz <130196123+Li0N77@users.noreply.github.com>
2024-08-31 02:45:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 echo "setup configuration"
 echo "{\n    \"SecurityTrails\": \"\",\n    \"Censys\": \"\",\n    \"Shodan\": \"\"\n}" > ~/.local/bin/lsubs-config.json
+
+mkdir -p ~/.local/bin
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    echo "adding path environment variable"
+    grep -qxF 'export PATH=$PATH:~/.local/bin/' ~/.bashrc || echo 'export PATH=$PATH:~/.local/bin/' >> ~/.bashrc
+    grep -qxF 'export PATH=$PATH:~/.local/bin/' ~/.zshrc || echo 'export PATH=$PATH:~/.local/bin/' >> ~/.zshrc
+    source ~/.bashrc
+    source ~/.zshrc
+fi
+
+echo "install lsubs"
+
 chmod +x lsubs
 cp lsubs ~/.local/bin/
-echo 'export PATH=$PATH:~/.local/bin/' >> ~/.bashrc
-echo 'export PATH=$PATH:~/.local/bin/' >> ~/.zshrc
 
 echo "Installing Python packages..."
 pip install -r requirements.txt
